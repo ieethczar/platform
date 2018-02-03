49af086b0db04bc5a884c63e875c8e256856def7
Cesar Gutierrez Espitia <ieethczar@gmail.com>
2018-02-02 23:21:18 -0600

    Modificaciones la menu

--- src/components/menu/MenuDisplay.js
+++ src/components/menu/MenuDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {Navbar,Nav,NavItem,MenuItem,NavDropdown,FormGroup,FormControl,Button} from 'react-bootstrap';
 
 export const MenuDisplay = () => (
@@ -12,7 +12,7 @@ export const MenuDisplay = () => (
 		<Navbar fixedTop collapseOnSelect>
 		  	<Navbar.Header>
 		    	<Navbar.Brand>
-		      		<a href="#brand">React-Bootstrap</a>
+		      		<Link to={"/"}>Platform</Link>
 		    	</Navbar.Brand>
 		    	<Navbar.Toggle />
 		  	</Navbar.Header>
@@ -26,21 +26,50 @@ export const MenuDisplay = () => (
 	    		<Nav>
 	      			<NavItem eventKey={1} href="#">Link</NavItem>
 	      			<NavItem eventKey={2} href="#">Link</NavItem>
-			      	<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-				        <MenuItem eventKey={3.1}>Action</MenuItem>
-				        <MenuItem eventKey={3.2}>Another action</MenuItem>
-				        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-				        <MenuItem divider />
-				        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-			      	</NavDropdown>
 	    		</Nav>
 	    		<Nav pullRight>
-			      	<NavItem eventKey={1} href="#">
-			        	Link Right
-			      	</NavItem>
 			      	<NavItem eventKey={2} href="#">
-			        	Link Right
+			        	Login
 			      	</NavItem>
+			      	<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+				        <MenuItem eventKey={3.1}>
+				        	<Link to={"/perfil"}>
+				        		Perfil
+				      		</Link>
+				      	</MenuItem>
+				        <MenuItem divider />
+				        <MenuItem eventKey={3.2}>
+				        	<Link to={"/admon"}>
+				        		Administrar productos
+				      		</Link>
+				      	</MenuItem>
+				      	<MenuItem eventKey={3.2}>
+				        	<Link to={"/admon"}>
+				        		Administrar ordenes
+				      		</Link>
+				      	</MenuItem>
+				      	<MenuItem eventKey={3.2}>
+				        	<Link to={"/admon"}>
+				        		Clientes Registrados
+				      		</Link>
+				      	</MenuItem>
+				        <MenuItem eventKey={3.2}>
+				        	<Link to={"/admon"}>
+				        		Administradores
+				      		</Link>
+				      	</MenuItem>
+				        <MenuItem eventKey={3.2}>
+				        	<Link to={"/admon"}>
+				        		Reportes
+				      		</Link>
+				      	</MenuItem>
+				        <MenuItem divider />
+				        <MenuItem eventKey={3.3}>
+				        	<Link to={"/admon"}>
+				        		Cerrar Sesión
+				      		</Link>
+				      	</MenuItem>
+			      	</NavDropdown>
 	    		</Nav>
 	  		</Navbar.Collapse>
 		</Navbar>
